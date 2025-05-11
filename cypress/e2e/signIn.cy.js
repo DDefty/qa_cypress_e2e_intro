91312f29e4e8c3e52a66e62373e62dd9e736fb4c
DDefty <dawid.kogut99@gmail.com>
2025-05-11 17:42:38 +0200

    test added

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/');
 
+    cy.contains('Sign in').click();
+    cy.get('input[type="email"]').type('fake1234@gmail.com');
+    cy.get('input[type="password"]').type('fake1234')
+
+    cy.get('button[type="submit"]').click();
+
+    cy.wait(2000)
+    cy.contains('fake1234').should('be.visible');
   });
 });
